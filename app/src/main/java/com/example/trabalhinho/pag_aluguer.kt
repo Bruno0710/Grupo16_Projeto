cd6b668760496aa4ad72510d5c94eaa042cc36d8
bruno <a22005283@mso364.ulp.pt>
2021-12-16 12:32:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/trabalhinho/pag_aluguer.kt
@@ -0,0 +1,11 @@
+package com.example.trabalhinho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pag_aluguer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pag_aluguer)
+    }
+}
